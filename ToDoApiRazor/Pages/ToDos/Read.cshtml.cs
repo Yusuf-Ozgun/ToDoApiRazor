7d65e7aecf199af48c4fa627ce09100a3509e419
Yusuf Ozgun <47563889+Yusuf-Ozgun@users.noreply.github.com>
2022-06-21 18:02:52 +0200

    edit read

--- ToDoApiRazor/Pages/ToDos/Read.cshtml.cs
+++ ToDoApiRazor/Pages/ToDos/Read.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToDoApiRazor.Data;
 
 namespace ToDoApiRazor.Pages.ToDos
 {
     public class ReadModel : PageModel
     {
-        public void OnGet()
+        private readonly DataContext _context;
+
+        public ReadModel(DataContext context) => _context = context;
+
+        public async void OnGet(uint id) => ToDo = await _context.ToDos.FindAsync(id);
+
+        public async void OnGetResolve(uint id)
         {
+            var ToDoToUpdate = _context.ToDos.SingleOrDefault(i => i.Id == id);
+            if (ToDoToUpdate == null) return;
+
+            ToDoToUpdate.LaatsteAanpassing = DateTime.Now;
+            _context.Update(ToDoToUpdate);
+            await _context.SaveChangesAsync();
         }
+
+        public ToDo? ToDo { get; private set; }
+
     }
 }
