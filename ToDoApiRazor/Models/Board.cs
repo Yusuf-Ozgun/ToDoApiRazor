4ca36cde29ff6c84bfcad818c59845a164160144
Yusuf Ozgun <47563889+Yusuf-Ozgun@users.noreply.github.com>
2022-06-21 13:37:40 +0200

    Delete ctors

--- ToDoApiRazor/Models/Board.cs
+++ ToDoApiRazor/Models/Board.cs
@@ -4,24 +4,22 @@ namespace EenVoudigeToDoApi.Models
 {
     public class Board
     {
-        #region Properties
         public int Id { get; set; }
         public string Name { get; set; }
         public List<ToDo> ToDos { get; set; }
 
 
-        public Board(int id, string name)
-        {
-            Id = id;
-            Name = name;
-            ToDos = new List<ToDo>();
-        }
-        public Board(int id, string name, List<ToDo> _todos)
-        {
-            Id=id;
-            Name = name;
-            ToDos = _todos;
-        }
-        #endregion
+        //public Board(int id, string name)
+        //{
+        //    Id = id;
+        //    Name = name;
+        //    ToDos = new List<ToDo>();
+        //}
+        //public Board(int id, string name, List<ToDo> _todos)
+        //{
+        //    Id=id;
+        //    Name = name;
+        //    ToDos = _todos;
+        //}
     }
 }
