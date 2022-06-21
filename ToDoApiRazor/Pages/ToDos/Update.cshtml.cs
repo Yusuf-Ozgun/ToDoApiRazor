4b37fae6d00a9daf26f86b1fc1b61328fff39ba4
Yusuf Ozgun <47563889+Yusuf-Ozgun@users.noreply.github.com>
2022-06-21 18:44:18 +0200

    Update

--- ToDoApiRazor/Pages/ToDos/Update.cshtml.cs
+++ ToDoApiRazor/Pages/ToDos/Update.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToDoApiRazor.Data;
 
 namespace ToDoApiRazor.Pages.ToDos
 {
     public class UpdateModel : PageModel
     {
-        public void OnGet()
+        DataContext _ctx;
+        public UpdateModel(DataContext ctx)
         {
+            _ctx = ctx;
+        }
+
+        [BindProperty]
+        public ToDos ToDos { get; set; }
+
+        public void OnGet(int? id)
+        {
+            if (id != null)
+            {
+                var data = (from todo in _ctx.ToDos
+                            where todo.Id == id
+                            select todo).SingleOrDefault();
+                ToDos = data;
+            }
         }
     }
 }
