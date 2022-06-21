22b97444d4a85cbd23a6482f677810ad68757495
Yusuf Ozgun <47563889+Yusuf-Ozgun@users.noreply.github.com>
2022-06-21 13:38:03 +0200

    comment ctors

--- ToDoApiRazor/Models/ToDo.cs
+++ ToDoApiRazor/Models/ToDo.cs
@@ -4,7 +4,6 @@ namespace EenVoudigeToDoApi.Models
 {
     public class ToDo
     {
-        #region Properties
         public int Id { get; set; }
         public string Titel { get; set; }
         public DateTime Creatie { get; set; }
@@ -12,17 +11,16 @@ namespace EenVoudigeToDoApi.Models
         public DateTime VervalDatum { get; set; }
         public int? UserId { get; set; }
         public User? User { get; set; }
-        #endregion
 
-        #region Ctor
-        public ToDo(int id, string titel, DateTime creatie, DateTime laatsteAanpassing, DateTime vervalDatum)
-        {
-            Id = id;
-            Titel = titel;
-            Creatie = creatie;
-            LaatsteAanpassing = laatsteAanpassing;
-            VervalDatum = vervalDatum;
-        }
-        #endregion
+        //#region Ctor
+        //public ToDo(int id, string titel, DateTime creatie, DateTime laatsteAanpassing, DateTime vervalDatum)
+        //{
+        //    Id = id;
+        //    Titel = titel;
+        //    Creatie = creatie;
+        //    LaatsteAanpassing = laatsteAanpassing;
+        //    VervalDatum = vervalDatum;
+        //}
+        //#endregion
     }
 }
