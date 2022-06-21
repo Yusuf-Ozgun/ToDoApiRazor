5c1d1125eed6a157453ac1bd049609742eb2b916
Yusuf Ozgun <47563889+Yusuf-Ozgun@users.noreply.github.com>
2022-06-21 13:38:28 +0200

    comment ctors

--- ToDoApiRazor/Models/User.cs
+++ ToDoApiRazor/Models/User.cs
@@ -4,28 +4,26 @@ namespace EenVoudigeToDoApi.Models
 {
     public class User
     {
-        #region Properties
         public int Id { get; set; }
         public string Name { get; set; }
         public List<ToDo> ToDos { get; set; }
         public List<Board> Boards { get; set; }
 
 
-        public User(int id, string name)
-        {
-            Id = id;
-            Name = name;
-            ToDos = new List<ToDo>();
-            Boards = new List<Board>();
+        //public User(int id, string name)
+        //{
+        //    Id = id;
+        //    Name = name;
+        //    ToDos = new List<ToDo>();
+        //    Boards = new List<Board>();
 
-        }
-        public User(int id, string name, List<ToDo> _todos, List<Board> _boards)
-        {
-            Id=id;
-            Name = name;
-            ToDos = _todos;
-            Boards = _boards;
-        }
-        #endregion
+        //}
+        //public User(int id, string name, List<ToDo> _todos, List<Board> _boards)
+        //{
+        //    Id=id;
+        //    Name = name;
+        //    ToDos = _todos;
+        //    Boards = _boards;
+        //}
     }
 }
