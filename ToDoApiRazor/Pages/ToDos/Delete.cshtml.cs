0719b4999d692c75f225704ced81ceba501dab7a
Yusuf Ozgun <47563889+Yusuf-Ozgun@users.noreply.github.com>
2022-06-21 18:40:27 +0200

    Delete

--- ToDoApiRazor/Pages/ToDos/Delete.cshtml.cs
+++ ToDoApiRazor/Pages/ToDos/Delete.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToDoApiRazor.Data;
 
 namespace ToDoApiRazor.Pages.ToDos
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly DataContext _ctx;
+
+        public ActionResult OnGetDelete(int? id)
         {
+            if (id != null)
+            {
+                var data = (from todo in _ctx.ToDos
+                            where todo.Id == id
+                            select todo).SingleOrDefault();
+
+                _ctx.Remove(data);
+                _ctx.SaveChanges();
+            }
+            return RedirectToPage("../Index");
         }
     }
 }
