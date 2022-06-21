53732fa74e001f7d0116c40d2fe2bcd79e1c1680
Yusuf Ozgun <47563889+Yusuf-Ozgun@users.noreply.github.com>
2022-06-21 13:59:19 +0200

    DataContext

---
+++ ToDoApiRazor/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using EenVoudigeToDoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApiRazor.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Board> Boards { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<ToDo> ToDos { get; set; }
+
+    }
+}
